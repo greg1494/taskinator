10382518eea6a59d18c76c8e888c2e3905c7268d
Greg Christiansen <gregchristiansen14@gmail.com>
2021-10-28 14:04:34 -0500

    dynamically create buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+var taskIdCounter = 0;
+
+
 var formE1 = document.querySelector("#task-form");
 var tasksToDoE1 = document.querySelector("#tasks-to-do");
 
@@ -26,11 +29,56 @@ var taskDataObj = {
 createTaskE1(taskDataObj);
 };
 
+var createTaskActions = function(taskId) {
+var actionContainerE1 = document.createElement("div");
+actionContainerE1.className = "task-actions";
+
+// create edit button
+var editButtonE1 = document.createElement("button");
+editButtonE1.textContent = "Edit";
+editButtonE1.className = "btn edit-btn";
+editButtonE1.setAttribute("data-task-id", taskId);
+
+actionContainerE1.appendChild(editButtonE1);
+
+// create delte button 
+var deleteButtonE1 = document.createElement("button");
+deleteButtonE1.textContent = "Delete";
+deleteButtonE1.className = "btn delete-btn";
+deleteButtonE1.setAttribute("data-task-id", taskId);
+
+actionContainerE1.appendChild(deleteButtonE1);
+
+var statusSelectE1 = document.createElement("select");
+statusSelectE1.className = "status-select";
+statusSelectE1.setAttribute("name", "status-change");
+statusSelectE1.setAttribute("data-task-id", taskId);
+
+actionContainerE1.appendChild(statusSelectE1);
+
+var statusChoices = ["To Do", "In Progress", "Completed"];
+for (var i = 0; i < statusChoices.length; i++) {
+    // create option Element
+    var statusOptionE1 = document.createElement("option");
+    statusOptionE1.textContent = statusChoices[i];
+    statusOptionE1.setAttribute("value", statusChoices[i]);
+
+    // append to select
+    statusSelectE1.appendChild(statusOptionE1);
+}
+
+return actionContainerE1;
+};
+
 var createTaskE1 = function(taskDataObj) {
 
 // create list item
 var listItemE1 = document.createElement("li");
 listItemE1.className = "task-item";
+
+// add task id as a custom attribute
+listItemE1.setAttribute("data-task-id", taskIdCounter);
+
 
 // create div to hold task info and add to list item
 var taskInfoE1 = document.createElement("div");
@@ -43,8 +91,14 @@ listItemE1.appendChild(taskInfoE1);
 
 console.dir(listItemE1);
 
+var taskActionsE1 = createTaskActions(taskIdCounter);
+listItemE1.appendChild(taskActionsE1);
+
 // add entire list item to list
 tasksToDoE1.appendChild(listItemE1);
+
+// increase task counter for next unique id 
+    taskIdCounter++;
 };
 
 formE1.addEventListener("click", taskFormHandler);
